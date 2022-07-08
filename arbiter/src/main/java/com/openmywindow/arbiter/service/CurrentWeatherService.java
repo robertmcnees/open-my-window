fd7013a59c5d3a52e96fe098242c6cd4fb78c1aa
Robert McNees <rmcnees@vmware.com>
2022-07-08 13:32:51 -0400

    Used properties to find URLs for other services

--- arbiter/src/main/java/com/openmywindow/arbiter/service/CurrentWeatherService.java
+++ arbiter/src/main/java/com/openmywindow/arbiter/service/CurrentWeatherService.java
@@ -2,17 +2,21 @@ package com.openmywindow.arbiter.service;
 
 import com.openmywindow.arbiter.record.CurrentWeatherRecord;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 public class CurrentWeatherService {
 
 	private final RestTemplate restTemplate;
 
+	@Value("${openweatherserviceurl")
+	private String openWeatherServiceUrl;
+
 	public CurrentWeatherService(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 	}
 
 	public CurrentWeatherRecord getCurrentWeather(Double lat, Double lon) {
-		return restTemplate.getForObject("http://localhost:8082/current/currentWeather?lat="+lat+"&lon="+lon, CurrentWeatherRecord.class);
+		return restTemplate.getForObject("http://" + openWeatherServiceUrl + "/current/currentWeather?lat="+lat+"&lon="+lon, CurrentWeatherRecord.class);
 	}
 }
--- arbiter/src/main/java/com/openmywindow/arbiter/service/GeocodeService.java
+++ arbiter/src/main/java/com/openmywindow/arbiter/service/GeocodeService.java
@@ -1,18 +1,26 @@
 package com.openmywindow.arbiter.service;
 
 import com.openmywindow.arbiter.record.GeocodeCoordinates;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 public class GeocodeService {
 
 	private final RestTemplate restTemplate;
+	private static final Logger log = LoggerFactory.getLogger(RestTemplate.class);
+
+	@Value("${geocodeserviceurl}")
+	private String geocodeServiceUrl;
 
 	public GeocodeService(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 	}
 
 	public GeocodeCoordinates getGeocodeCoordinates(String postalCode) {
-		return restTemplate.getForObject("http://localhost:8081/geocode/coordinates?postalCode=" + postalCode, GeocodeCoordinates.class);
+		log.info("Geocode URL" + geocodeServiceUrl);
+		return restTemplate.getForObject("http://" + geocodeServiceUrl + "/geocode/coordinates?postalCode=" + postalCode, GeocodeCoordinates.class);
 	}
 }
