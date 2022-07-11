2a114d29405db43d906e9c31f57b132c731f94a9
Robert McNees <rmcnees@vmware.com>
2022-07-11 15:37:57 -0400

    Arbiter engine provides better messaging

--- arbiter/src/main/java/com/openmywindow/arbiter/ArbiterEngine.java
+++ arbiter/src/main/java/com/openmywindow/arbiter/ArbiterEngine.java
@@ -10,26 +10,35 @@ public class ArbiterEngine {
 	private static final Logger log = LoggerFactory.getLogger(ArbiterEngine.class);
 
 	public WindowRecord determineWindowStatus(CurrentWeatherRecord dailyWeather) {
-		log.info("Current temp = " + dailyWeather.temp());
-		log.info("High temp = " + dailyWeather.maxTemp());
-		log.info("Low temp = " + dailyWeather.minTemp());
+
+		String windowStatus = "";
 
 		if (dailyWeather.maxTemp() < 65) {
-			return new WindowRecord("closed - too cold");
+			windowStatus = "Brr.... Keep the window closed today.  It is currently " + dailyWeather.temp() + " and the high will only be " + dailyWeather.maxTemp() +
+				" with a low of " + dailyWeather.minTemp();
+		}
+		else if (dailyWeather.minTemp() > 75) {
+			windowStatus = "Keep the windows closed.  Use the AC today because the temp is " + dailyWeather.temp() + " and it isn't getting lower than " +
+					dailyWeather.minTemp() + ".  But it's going to be a high of " + dailyWeather.maxTemp();
+		}
+		else if (dailyWeather.temp() > 75) {
+			windowStatus = "It's hot outside.  Close the windows because the temp is " + dailyWeather.temp() + " with a high of " +
+					dailyWeather.maxTemp() + ".  The low will be " + dailyWeather.minTemp() + ".";
+		}
+		else if (dailyWeather.temp() > 65 && dailyWeather.temp() < 75) {
+			windowStatus = "Open up the windows!  It is a great temperature outside at " + dailyWeather.temp() +".  The low is " + dailyWeather.minTemp() +
+					" with a high of " + dailyWeather.maxTemp() +".";
+		}
+		else if (dailyWeather.temp() < 65 && dailyWeather.maxTemp() > 70) {
+			windowStatus = "Free air conditioning?  It is " + dailyWeather.temp() + " outside now but it will heat up to " + dailyWeather.maxTemp() +
+					".  The low today is " + dailyWeather.minTemp() + ".  Get the free cooling inside while you can.";
+		}
+		else {
+			windowStatus = "Not sure what to do with a temp of " + dailyWeather.temp() + " with a low of " + dailyWeather.minTemp() +
+					" and a high of " + dailyWeather.maxTemp();
 		}
 
-		if (dailyWeather.minTemp() > 75) {
-			return new WindowRecord("closed - too hot");
-		}
+		return new WindowRecord(windowStatus);
 
-		if (dailyWeather.temp() > 65 && dailyWeather.temp() < 75) {
-			return new WindowRecord("open - good temp outside");
-		}
-
-		if (dailyWeather.temp() < 65 && dailyWeather.maxTemp() > 70) {
-			return new WindowRecord("open - cold now, hot later");
-		}
-
-		return new WindowRecord("closed - no scenario");
 	}
 }
