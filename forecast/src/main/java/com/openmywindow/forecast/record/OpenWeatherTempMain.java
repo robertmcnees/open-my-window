50d0938df0a4d9a20389487036c44e198ca2c978
Robert McNees <rmcnees@vmware.com>
2022-07-14 15:18:36 -0400

    Simplified record object to remove unused variables

--- forecast/src/main/java/com/openmywindow/forecast/record/OpenWeatherTempMain.java
+++ forecast/src/main/java/com/openmywindow/forecast/record/OpenWeatherTempMain.java
@@ -1,4 +1,4 @@
 package com.openmywindow.forecast.record;
 
-public record OpenWeatherTempMain(Double temp, Double feels_like, Double temp_min, Double temp_max, Double pressure, Double humidity) {
+public record OpenWeatherTempMain(Double temp, Double temp_min, Double temp_max, Double humidity) {
 }
