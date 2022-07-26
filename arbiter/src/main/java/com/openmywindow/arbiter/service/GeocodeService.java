8630fc4e5ae8d9840aefe0dc7c1ef66c0e3ee829
Robert McNees <rmcnees@vmware.com>
2022-07-26 14:48:21 -0400

    Use Azure Spring Apps k8s service for service discovery

--- arbiter/src/main/java/com/openmywindow/arbiter/service/GeocodeService.java
+++ arbiter/src/main/java/com/openmywindow/arbiter/service/GeocodeService.java
@@ -6,27 +6,23 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class GeocodeService {
 
-	private final RestTemplate restTemplate;
 	private static final Logger log = LoggerFactory.getLogger(RestTemplate.class);
-	private final DiscoveryClient discoveryClient;
+	private final RestTemplate restTemplate;
+	@Value("${geocodeserviceurl:geocode}")
+	private String geocodeServiceUrl;
 
-	public GeocodeService(RestTemplateBuilder restTemplateBuilder, DiscoveryClient discoveryClient) {
+	public GeocodeService(RestTemplateBuilder restTemplateBuilder) {
 		this.restTemplate = restTemplateBuilder.build();
-		this.discoveryClient = discoveryClient;
 	}
 
 	public GeocodeCoordinates getGeocodeCoordinates(String postalCode) {
-		ServiceInstance serviceInstance = discoveryClient.getInstances("geocode").get(0);
-		log.info("Geocode URL" + serviceInstance.getUri().toString());
-		return restTemplate.getForObject("http://" + serviceInstance.getUri().getHost() + ":" + serviceInstance.getUri().getPort()
-				+ "/geocode/coordinates?postalCode=" + postalCode, GeocodeCoordinates.class);
+		log.info("Geocode URL" + geocodeServiceUrl);
+		return restTemplate.getForObject("http://" + geocodeServiceUrl + "/geocode/coordinates?postalCode=" + postalCode, GeocodeCoordinates.class);
 	}
 }
