2fbe5c3d196f1b3cb3ac1dc9628db403d4473d68
Robert McNees <rmcnees@vmware.com>
2022-12-12 15:23:57 -0500

    Deleted UI module

--- ui/README.md
+++
@@ -1,9 +0,0 @@
-#Overview
-
-### Building for Production
-
-When preparing to deploy the app to production, use the `production` profile.  This is becuase Vaadin requires configuration whendeploying to a production environment.
-
-```
-mvn clean package -Pproduction
-```
--- ui/src/main/java/com/openmywindow/ui/record/WindowRecord.java
+++
@@ -1,3 +0,0 @@
-package com.openmywindow.ui.record;
-
-public record WindowRecord(String status, String tempRecommendation, String humidityRecommendation, String nextRecommendation, Double currentTemp, Double currentHumidity, Double dailyHighTemp, Double dailyLowTemp) { }
