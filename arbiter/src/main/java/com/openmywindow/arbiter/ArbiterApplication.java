74ebc7430820742f3963a9a4b79850e8ba1a9ed2
Robert McNees <rmcnees@vmware.com>
2022-12-19 13:12:53 -0500

    Upgrade to Boot 3 and Cloud 2022

--- arbiter/src/main/java/com/openmywindow/arbiter/ArbiterApplication.java
+++ arbiter/src/main/java/com/openmywindow/arbiter/ArbiterApplication.java
@@ -2,10 +2,8 @@ package com.openmywindow.arbiter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ArbiterApplication {
 
 	public static void main(String[] args) {
--- forecast/src/main/java/com/openmywindow/forecast/ForecastApplication.java
+++ forecast/src/main/java/com/openmywindow/forecast/ForecastApplication.java
@@ -2,10 +2,8 @@ package com.openmywindow.forecast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ForecastApplication {
 
 	public static void main(String[] args) {
