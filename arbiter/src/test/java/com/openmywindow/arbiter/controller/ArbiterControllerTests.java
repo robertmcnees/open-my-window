191942284f6018b15c956ef2a66caa1dd3a5f7f2
Robert McNees <rmcnees@vmware.com>
2023-01-11 14:27:17 -0500

    Updated failing tests in arbiter

--- arbiter/src/test/java/com/openmywindow/arbiter/controller/ArbiterControllerTests.java
+++ arbiter/src/test/java/com/openmywindow/arbiter/controller/ArbiterControllerTests.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(profiles = "local")
 public class ArbiterControllerTests {
 
 	@LocalServerPort
